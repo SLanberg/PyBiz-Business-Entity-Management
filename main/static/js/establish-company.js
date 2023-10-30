902e6e0e324ac21ee4c02fac2780a061d9e40b19
Stephan Lanberg <stephan.lanberg@stellarforce.net>
2023-10-30 21:58:15 +0200

    Add first steps for the JavaScript hiding via select field

--- main/static/js/establish-company.js
+++ main/static/js/establish-company.js
@@ -24,18 +24,45 @@ $(document).ready(function () {
     $("#id_" + formsetPrefix + "-TOTAL_FORMS").val(parseInt(formIdx) + 1);
   });
 
-
   function updateTotalForms() {
     var totalFormsInput = $("#id_shareholder_set-TOTAL_FORMS");
     var currentTotalForms = parseInt(totalFormsInput.val());
     totalFormsInput.val(currentTotalForms - 1);
-    
-}
-
+  }
 
   $(document).on("click", ".remove-form", function () {
     $(this).closest(".form-container").remove();
     updateTotalForms();
   });
 
+
+  function toggleShareholderFields() {
+    var entitySelect = $("#id_shareholder_set-0-entity_type");
+    var founderStatus = entitySelect.val();
+    var naturalFields = $("#id_shareholder_set-0-natural_person, label[for='id_shareholder_set-0-natural_person']");
+    var legalEntityFields = $("#id_shareholder_set-0-legal_entity, label[for='id_shareholder_set-0-legal_entity']");
+
+    if (founderStatus === "natural_person") {
+        naturalFields.css("display", "block");
+        legalEntityFields.css("display", "none");
+        // Clear the values of the legal entity fields
+        $("#id_shareholder_set-0-legal_entity").val("");
+    } else {
+        naturalFields.css("display", "none");
+        legalEntityFields.css("display", "block");
+        // Clear the values of the natural person fields
+        $("#id_shareholder_set-0-natural_person").val("");
+    }
+}
+
+// Initial state on page load
+toggleShareholderFields();
+
+// Listen for changes in founder status dropdown
+$("#id_shareholder_set-0-entity_type").on("change", function () {
+    toggleShareholderFields();
+});
+
+
+  
 });
